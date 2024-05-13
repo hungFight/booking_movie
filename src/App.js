40e60f1fd643d53fff700ef6b9fab3f23c61c284
hungaa1 <nguyentronghung05072003@gmail.com>
2024-05-13 14:31:55 +0700

    add login and register

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import SignInSide from './Authentication/Login';
+import { Route, Routes } from 'react-router-dom';
+import Register from './Authentication/Register';
+import { Image } from 'antd';
+import Images from './assets/images/image';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="w-40 h-40 absolute top-3 right-9">
+                <img src={Images.logoApp} alt="Cinema" className="w-full h-full" />
+            </div>{' '}
+            <Routes>
+                <Route path="/login" element={<SignInSide />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/assets/images/image.js
@@ -0,0 +1,13 @@
+import login from './ingdom-of-the-Planet-of-Apes--608x800-a0cbf360-0a8a-11ef-b78a-95022c935f66.avif';
+import loginTwo from './dd6stao-f94e74d4-93a4-4f28-b0e9-b992db46c2ae.jpg';
+import app from './2529d463-0831-45c4-8099-2e4fb6d526fd.png';
+import register from './s5HQf2Gb3lIO2cRcFwNL9sn1o1o.jpg';
+import registerTwo from './godzilla_x_kong_the_new_empire_poster_4k_by_andrewvm_dgmztse-fullview.jpg';
+const Images = {
+    logoLogin: login,
+    logoApp: app,
+    register,
+    registerTwo,
+    loginTwo,
+};
+export default Images;
