cd28d9044af7767bb4b9ab85104f8f5f47e41acb
hungaa1 <nguyentronghung05072003@gmail.com>
2024-06-06 15:52:41 +0700

    reset

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import SignInSide from './Authentication/Login';
 import { Route, Routes } from 'react-router-dom';
 import Register from './Authentication/Register';
-import { Image } from 'antd';
 import Images from './assets/images/image';
 import ResetPassword from './Authentication/ResetPassword';
 function App() {
     return (
         <div className="App">
             <div className="w-40 h-40 absolute top-3 right-9">
-                <img src={Images.logoApp} alt="Cinema" className="w-full h-full" />
-            </div>{' '}
+                <img src={ Images.logoApp } alt="Cinema" className="w-full h-full" />
+            </div>{ ' ' }
             <Routes>
-                <Route path="/login" element={<SignInSide />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/resetPassword" element={<ResetPassword />} />
+                <Route path="/login" element={ <SignInSide /> } />
+                <Route path="/register" element={ <Register /> } />
+                <Route path="/resetPassword" element={ <ResetPassword /> } />
             </Routes>
         </div>
     );
