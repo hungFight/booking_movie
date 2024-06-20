04468d5cc7a2e152f5ce7d1e77aa8cf3ec9f3373
Duckun1909 <duc.ph.2045@aptechlearning.edu.vn>
2024-06-20 14:34:30 +0700

    homepage

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import './App.css';
 import SignInSide from './Authentication/Login';
 import { Route, Routes } from 'react-router-dom';
 import Register from './Authentication/Register';
-import { Image } from 'antd';
+import { Image, Layout } from 'antd';
 import Images from './assets/images/image';
+import LayoutHeaderFooter from './Components/layout/LayoutHeaderFooter/LayoutHeaderFooter';
+import Homepage from './pages/Homepage';
 function App() {
     return (
         <div className="App">
-            <div className="w-40 h-40 absolute top-3 right-9">
+            <div className="w-40 hidden h-40 absolute top-3 right-9">
                 <img src={Images.logoApp} alt="Cinema" className="w-full h-full" />
             </div>{' '}
             <Routes>
                 <Route path="/login" element={<SignInSide />} />
                 <Route path="/register" element={<Register />} />
+                <Route path='/homepage' element={<LayoutHeaderFooter><Homepage/></LayoutHeaderFooter>} />
             </Routes>
         </div>
     );
